bb84b91900d51c6c902f97a4503af1bef18f49a1
yuki-zirushi <yuko.12.151@gmail.com>
2021-05-04 12:05:18 +0000

    [Add] validates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,17 +17,20 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    if book.save
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was succcessfully created."
+      redirect_to "/books/#{book.id}"
+    else
+      render("books/index")
     end
-    redirect_to "/books/#{book.id}"
   end
   
   def update
     book = Book.find(params[:id])
     if book.update(book_params)
-      flash[:notice] = "Book was successufully updated"
+      flash[:notice] = "Book was successufully updated."
     end
     redirect_to "/books/#{book.id}"
   end
